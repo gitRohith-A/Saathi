c109307e1d287deddaa29f5de582c6eec96da96d
Shuheb <mshuheb8@gmail.com>
2024-01-28 16:38:09 +0530

    removed the deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
-    images: {
-        unoptimized: true
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
